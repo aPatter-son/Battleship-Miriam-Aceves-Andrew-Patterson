81bcf9e099babf0225e529c7227e474d1334d47c
Miriam Aceves <miriammerari02@gmail.com>
2021-07-15 19:31:21 -0400

    Write test for render method and fix fire upon test

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -1,3 +1,4 @@
+require 'pry'
 require './lib/ship'
 require './lib/cell'
 
@@ -32,12 +33,45 @@ RSpec.describe Cell do
     cruiser = Ship.new("Cruiser", 3)
 
     cell.place_ship(cruiser)
+
     expect(cell.ship).to eq(cruiser)
   end
 
-  xit 'checks if empty' do
+  it 'checks if empty' do
     cell = Cell.new("B4")
 
-    expect(cell.empty?).to eq(false)
+    expect(cell.empty?).to eq(true)
+  end
+
+  it 'fired_upon?' do
+    cell = Cell.new("B4")
+    cruiser = Ship.new("Cruiser", 3)
+
+    # binding.pry
+    expect(cell.fired_upon?).to eq(false)
+  end
+
+  it 'fire_upon' do
+    cell = Cell.new("B4")
+    cruiser = Ship.new("Cruiser", 3)
+
+    cell.place_ship(cruiser)
+
+    cell.fire_upon
+
+    expect(cruiser.health).to eq(2)
+  end
+
+  it 'renders' do
+    cell_1 = Cell.new("B4")
+
+    expect(cell_1.render).to eq(".")
+
+    cell_2 = Cell.new("C3")
+    cruiser = Ship.new("Cruiser", 3)
+
+    cell_2.place_ship(cruiser)
+
+    expect(cell_2.render).to eq(".")
   end
 end
