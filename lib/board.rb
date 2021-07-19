09538ed523885dc99b8577d7f6135a96e44f8ba4
Miriam Aceves <miriammerari02@gmail.com>
2021-07-19 19:58:07 -0400

    Begin creating render method

--- lib/board.rb
+++ lib/board.rb
@@ -38,7 +38,6 @@ class Board
     coordinates.map do |number|
       # binding.pry
       number.coordinate[1].to_i
-
     end.uniq
   end
 
@@ -58,10 +57,29 @@ class Board
 
   def cons_letters
     # incomplete
-    letter.each do |let|
+    ord_values = letters.map do |let|
       let.ord
 
       # ord[] -> each_cons (rules) [1,2,3]
     end
+  end
+
+
+  def render
+    # user board
+    "  1 2 3 4 \n" +
+    "A #{@cells["A1"].render(true)} #{@cells["A2"].render(true)} #{@cells["A3"].render(true)} #{@cells["A4"].render(true)}"
+    "B #{@cells["B1"].render(true)} #{@cells["B2"].render(true)} #{@cells["B3"].render(true)} #{@cells["B4"].render(true)}"
+    "C #{@cells["C1"].render(true)} #{@cells["C2"].render(true)} #{@cells["C3"].render(true)} #{@cells["C4"].render(true)}"
+    "D #{@cells["D1"].render(true)} #{@cells["D2"].render(true)} #{@cells["D3"].render(true)} #{@cells["D4"].render(true)}"
+
+    #computer board
+    "  1 2 3 4 \n" +
+    "A #{@cells["A1"].render} #{@cells["A2"].render} #{@cells["A3"].render} #{@cells["A4"].render}"
+    "B #{@cells["B1"].render} #{@cells["B2"].render} #{@cells["B3"].render} #{@cells["B4"].render}"
+    "C #{@cells["C1"].render} #{@cells["C2"].render} #{@cells["C3"].render} #{@cells["C4"].render}"
+    "D #{@cells["D1"].render} #{@cells["D2"].render} #{@cells["D3"].render} #{@cells["D4"].render}"
+
+
   end
 end
